f9d77c9d7edd77ab32accd7f05f980ebac66309d
Mindaugas Minelga <minelga.m@gmail.com>
2023-10-12 21:51:52 +0300

    add main.py file

---
+++ main.py
@@ -0,0 +1,52 @@
+# for number in range(1, 7):
+#     if number != 3:
+#         print(number)
+
+
+# username = "Vytautas"
+# password = "8888"
+
+# while True:
+#     user_input = input("Enter your username and password: ")
+#     if user_input == username + " " + password:
+#         break
+
+# print(f"You entered correct username and password")
+
+
+# new_list = []
+# for _ in range(10):
+#     input_new = input("Enter an integer: ")
+#     number = int(input_new)
+#     new_list.append(number)
+
+# print(f"The sum of numbers is: {sum(new_list)}, and average is: {sum(new_list)/10}")
+
+# from random import randrange
+# new_dict = {}
+# for key in range(1, 11):
+#     value = randrange(1, 101)
+#     new_dict[key] = value
+
+# print(new_dict)
+
+# pin_code = 2145
+
+# for pin in range(1, 10000):
+#     if pin == pin_code:
+#         break
+    
+# print(f"The pin code is: {pin}")
+
+# def cooking_time(eggs):
+#     if eggs == 0:
+#         return 0
+#     elif eggs < 8:
+#         return 5
+#     elif eggs % 8 == 0:
+#         return eggs / 8 * 5
+#     else:
+#         return ((eggs // 8) * 5) + 5
+    
+# print(cooking_time(34456))
+
